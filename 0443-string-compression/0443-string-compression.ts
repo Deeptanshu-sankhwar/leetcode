b1ac46d310ff45a1e53b2d7815966b0a097ab2e7
Deeptanshu Sankhwar <desa4692@colorado.edu>
2025-03-08 05:13:27 -0700

    Time: 3 ms (43.6%), Space: 58.9 MB (22.09%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.ts
@@ -0,0 +1,24 @@
+function compress(chars: string[]): number {
+    let write = 0;
+    let read = 0;
+
+    while (read < chars.length) {
+        let char = chars[read];
+        let count = 0;
+
+        while (read < chars.length && chars[read] === char) {
+            read++;
+            count++;
+        }
+
+        chars[write++] = char;
+
+        if (count > 1) {
+            for (let digit of count.toString()) {
+                chars[write++] = digit;
+            }
+        }
+    }
+
+    return write;
+}
